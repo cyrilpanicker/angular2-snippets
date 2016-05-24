d821b0aff2cffa6076f127aa901a498da8f7892a
cyrilpanicker <cyrilpanicker@gmail.com>
2016-05-24 15:04:51 +0530

    added changes

--- app/forms-demo/forms-demo.ts
+++ app/forms-demo/forms-demo.ts
@@ -64,24 +64,56 @@ export class App3{
 })
 export class App4{
     submit(){
-        console.log('submitted');
+        console.log('form submitted');
     }
 }
 //-------------------------------------------------
-
+@Component({
+    selector:'app',
+    template:`
+        <div ngForm #formControl="ngForm" >
+            <input ngControl #inputTextbox #inputControl="ngForm" required />
+            <span [hidden]="inputControl.valid || inputControl.pristine" >Required Field, </span>
+            {{inputTextbox.className}}
+            <button [disabled]="!formControl.valid" (click)="submitForm()" >Submit</button>
+        </div>
+    `
+})
+export class App5{
+    submitForm(){
+        console.log('form submitted');
+    }
+}
+//-------------------------------------------------
 @Component({
     selector:'app',
     template:`
         <form (ngSubmit)="submitForm()" #formControl="ngForm" >
             <input ngControl="inputControl" #inputTextbox required />
-            <span [hidden]="formControl.form.controls.inputControl && (formControl.form.controls.inputControl.valid || formControl.form.controls.inputControl.pristine)" 
-             >Required Field, </span>
+            <span [hidden]="formControl.form.controls.inputControl && (formControl.form.controls.inputControl.valid || formControl.form.controls.inputControl.pristine)">Required Field, </span>
             {{inputTextbox.className}}
             <button [disabled]="formControl.form.controls.inputControl && !formControl.form.controls.inputControl.valid" >Submit</button>
         </form>
     `
 })
-export class App5{
+export class App6{
+    submitForm(){
+        console.log('form submitted');
+    }
+}
+//-------------------------------------------------
+@Component({
+    selector:'app',
+    template:`
+        <div ngForm #formControl="ngForm" >
+            <input ngControl="inputControl" #inputTextbox required />
+            <span [hidden]="formControl.form.controls.inputControl && (formControl.form.controls.inputControl.valid || formControl.form.controls.inputControl.pristine)">Required Field, </span>
+            {{inputTextbox.className}}
+            <button [disabled]="formControl.form.controls.inputControl && !formControl.form.controls.inputControl.valid" (click)="submitForm()" >Submit</button>
+        </form>
+    `
+})
+export class App7{
     submitForm(){
         console.log('form submitted');
     }
